5ffdbc91d832cd8ea48bef0ea946e37f1f28cb0b
nanikit <nanikit4@gmail.com>
2022-11-21 20:52:16 +0900

    fix: remove artifact after refresh

--- src/app.tsx
+++ src/app.tsx
@@ -87,74 +87,71 @@ function ConnectedOverlay({ state }: { state: OverlayState }) {
   const fittedTextRef = useRef<HTMLParagraphElement>(null);
   useTextFit({ ref: fittedTextRef, maxHeight: vw100 * 0.078, maxSize: vw100 * 0.06 });
 
-  if (!hash) {
-    return (
+  return (
+    <>
+      <div
+        className={`w-full h-full transition duration-1000 flex leading-[1.2]${
+          isRight ? " flex-row" : " flex-row-reverse"
+        }${!mapInfo ? " opacity-0" : ""}`}
+      >
+        <div className={`z-0 flex-1 overflow-hidden pr-[0.05em]`}>
+          <div
+            className={`h-full transition duration-500 flex flex-col${isRight ? " items-end" : ""}${
+              !mapInfo ? " translate-x-[100%]" : ""
+            }`}
+          >
+            <p
+              ref={fittedTextRef}
+              className={`flex flex-row flex-wrap gap-[0_0.2em] items-start leading-[1] ${
+                isRight ? "justify-end" : ""
+              }`}
+            >
+              <span className="text-[0.5em] [-webkit-text-stroke:0.05em_black] leading-[1.4]">
+                {subtitle ?? ""}
+              </span>
+              <span className="[-webkit-text-stroke:0.03em_black] break-keep text-right">
+                {title ?? ""}
+              </span>
+            </p>
+            <div
+              className={`flex-1 flex flex-col flex-wrap justify-end ${isRight ? "items-end" : ""}`}
+            >
+              <p className="text-[0.16em] [-webkit-text-stroke:0.04em_black] mt-[0.2em]">
+                {artist} [{mapper}]
+              </p>
+              <div className="flex items-end gap-[0.05em] mt-[0.03em]">
+                {!!beatmap?.id && (
+                  <p className="text-[0.14em] leading-[1] [-webkit-text-stroke:0.035em_black]">
+                    !bsr {beatmap.id}
+                  </p>
+                )}
+                {!!difficulty && (
+                  <DifficultyLabel difficulty={difficulty} characteristic={characteristic} />
+                )}
+              </div>
+            </div>
+            <AutoProgressBar
+              duration={duration ?? 1}
+              progress={progress ?? lastProgress}
+              className={`shrink-0 h-[0.07em] mt-[0.04em] w-full ${
+                (scoring?.health ?? lastScoring?.health ?? 0) > 0
+                  ? "[--color-primary:#eee]"
+                  : "[--color-primary:#555]"
+              }`}
+            />
+          </div>
+        </div>
+        <TransparentFallbackImg
+          src={coverUrl}
+          className="z-10 aspect-square object-cover h-full rounded-[0.1em]"
+        />
+      </div>
       <div
         className={
           `absolute top-[0.05em] right-[0.05em] w-[0.1em] h-[0.1em] rounded-full` +
           ` bg-gradient-to-br from-green-300 to-emerald-600 animate-[0.2s_ease-in_1s_forwards_onetime-fadeout]`
         }
       />
-    );
-  }
-
-  return (
-    <div
-      className={`w-full h-full transition duration-1000 flex leading-[1.2]${
-        isRight ? " flex-row" : " flex-row-reverse"
-      }${!mapInfo ? " opacity-0" : ""}`}
-    >
-      <div className={`z-0 flex-1 overflow-hidden pr-[0.05em]`}>
-        <div
-          className={`h-full transition duration-500 flex flex-col${isRight ? " items-end" : ""}${
-            !mapInfo ? " translate-x-[100%]" : ""
-          }`}
-        >
-          <p
-            ref={fittedTextRef}
-            className={`flex flex-row flex-wrap gap-[0_0.2em] items-start leading-[1] ${
-              isRight ? "justify-end" : ""
-            }`}
-          >
-            <span className="text-[0.5em] [-webkit-text-stroke:0.05em_black] leading-[1.4]">
-              {subtitle ?? ""}
-            </span>
-            <span className="[-webkit-text-stroke:0.03em_black] break-keep text-right">
-              {title ?? ""}
-            </span>
-          </p>
-          <div
-            className={`flex-1 flex flex-col flex-wrap justify-end ${isRight ? "items-end" : ""}`}
-          >
-            <p className="text-[0.16em] [-webkit-text-stroke:0.04em_black] mt-[0.2em]">
-              {artist} [{mapper}]
-            </p>
-            <div className="flex items-end gap-[0.05em] mt-[0.03em]">
-              {!!beatmap?.id && (
-                <p className="text-[0.14em] leading-[1] [-webkit-text-stroke:0.035em_black]">
-                  !bsr {beatmap.id}
-                </p>
-              )}
-              {!!difficulty && (
-                <DifficultyLabel difficulty={difficulty} characteristic={characteristic} />
-              )}
-            </div>
-          </div>
-          <AutoProgressBar
-            duration={duration ?? 1}
-            progress={progress ?? lastProgress}
-            className={`shrink-0 h-[0.07em] mt-[0.04em] w-full ${
-              (scoring?.health ?? lastScoring?.health ?? 0) > 0
-                ? "[--color-primary:#eee]"
-                : "[--color-primary:#555]"
-            }`}
-          />
-        </div>
-      </div>
-      <TransparentFallbackImg
-        src={coverUrl}
-        className="z-10 aspect-square object-cover h-full rounded-[0.1em]"
-      />
-    </div>
+    </>
   );
 }
