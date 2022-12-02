69b5946eebc42a9cec57f6429acb46188714eb06
nanikit <nanikit4@gmail.com>
2022-12-03 05:22:29 +0900

    chore: preserve jotai state during fast refresh

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import cloudflarePagesFunctions from 'vite-plugin-cloudflare-functions';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cloudflarePagesFunctions from "vite-plugin-cloudflare-functions";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflarePagesFunctions({ dts: 'cloudflare.d.ts.log' })],
+  plugins: [
+    react({
+      babel: { plugins: ["jotai/babel/plugin-react-refresh"] },
+    }),
+    cloudflarePagesFunctions({ dts: "cloudflare.d.ts.log" }),
+  ],
 });
