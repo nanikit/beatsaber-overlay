5b70aac9532c69843a8a9c6d1666d6b72199773e
nanikit <nanikit4@gmail.com>
2024-03-14 22:35:42 +0900

    chore: update mock script

--- tool/bsplus_server_mock.ts
+++ tool/bsplus_server_mock.ts
@@ -1,9 +1,11 @@
-import { delay } from "https://deno.land/std@0.165.0/async/delay.ts";
+import { delay } from "https://deno.land/std@0.219.0/async/delay.ts";
 import { BeatsaverMap } from "../src/modules/beatsaver.ts";
 
 let beatmaps: BeatsaverMap[] = [];
 
-main().catch(console.error);
+if (import.meta.main) {
+  main().catch(console.error);
+}
 
 async function main() {
   const response = await fetch("https://beatsaver.com/api/search/text/0?sortOrder=Relevance");
@@ -11,20 +13,12 @@ async function main() {
   beatmaps = json.docs as BeatsaverMap[];
 
   const port = 2947;
-  console.log(`Preloaded map data. Listen on ${port}..`);
-  for await (const connection of Deno.listen({ port })) {
-    handleHttp(connection);
-  }
+  const server = Deno.serve({ port }, handleRequest);
+  await server.finished;
 }
 
-async function handleHttp(connection: Deno.Conn) {
-  for await (const request of Deno.serveHttp(connection)) {
-    handleRequest(request);
-  }
-}
-
-async function handleRequest(request: Deno.RequestEvent) {
-  const { socket, response } = Deno.upgradeWebSocket(request.request);
+function handleRequest(request: Request) {
+  const { socket, response } = Deno.upgradeWebSocket(request);
   socket.onopen = async () => {
     console.log(`[${new Date().toISOString()}] accepted websocket.`);
     socket.send(
@@ -35,107 +29,100 @@ async function handleRequest(request: Deno.RequestEvent) {
     }
     console.log(`readyState is ${socket.readyState}, break.`);
   };
-  await request.respondWith(response);
+  return response;
 }
 
 async function sendActivity(socket: WebSocket) {
-  if (socket.readyState !== socket.OPEN) {
+  if (isSocketClosed()) {
     return;
   }
 
-  socket.send(
-    `{ "_type": "event", "_event": "gameState", "gameStateChanged": "Menu" }`,
-  );
-
-  socket.send(
-    `{ "_type": "event", "_event": "gameState", "gameStateChanged": "Playing" }`,
-  );
-  socket.send(
-    `{ "_type": "event", "_event": "resume", "resumeTime": 0.01756902 }`,
-  );
+  socket.send(`{ "_type": "event", "_event": "gameState", "gameStateChanged": "Menu" }`);
+  socket.send(`{ "_type": "event", "_event": "gameState", "gameStateChanged": "Playing" }`);
+  socket.send(`{ "_type": "event", "_event": "resume", "resumeTime": 0.01756902 }`);
   socket.send(
     `{ "_type": "event", "_event": "score", "scoreEvent": { "time": 0.01756902, "score": 0, "accuracy": 1.0, "combo": 0, "missCount": 0, "currentHealth": 0.5 } }`,
   );
-  const map = beatmaps[Math.floor(Math.random() * beatmaps.length)];
-  const { diffs, hash, coverURL } = map.versions[0];
-  const diff = diffs[Math.floor(Math.random() * diffs.length)];
-  socket.send(
-    JSON.stringify({
-      _type: "event",
-      _event: "mapInfo",
-      mapInfoChanged: {
-        level_id: `custom_level_${hash.toUpperCase()}${
-          Math.random() >= 0.5 ? "_245de (NanaCat - Glorious Crown)" : ""
-        }`,
-        name: map.metadata.songName,
-        sub_name: map.metadata.songSubName,
-        artist: map.metadata.songAuthorName,
-        mapper: map.metadata.levelAuthorName,
-        characteristic: diff.characteristic,
-        difficulty: diff.difficulty,
-        duration: map.metadata.duration * 1000,
-        time: 0.1,
-        timeMultiplier: 1,
-        BPM: map.metadata.bpm,
-        PP: 0,
-        BSRKey: map.id,
-        coverRaw: coverURL,
-      },
-    }),
-  );
+  const map = getRandomElement(beatmaps);
+  const { diffs, hash, coverURL } = map.versions[0]!;
+  const diff = getRandomElement(diffs);
+  socket.send(JSON.stringify({
+    _type: "event",
+    _event: "mapInfo",
+    mapInfoChanged: {
+      level_id: `custom_level_${hash.toUpperCase()}${
+        Math.random() >= 0.5 ? "_245de (NanaCat - Glorious Crown)" : ""
+      }`,
+      name: map.metadata.songName,
+      sub_name: map.metadata.songSubName,
+      artist: map.metadata.songAuthorName,
+      mapper: map.metadata.levelAuthorName,
+      characteristic: diff.characteristic,
+      difficulty: diff.difficulty,
+      duration: map.metadata.duration * 1000,
+      time: 0.1,
+      timeMultiplier: 1,
+      BPM: map.metadata.bpm,
+      PP: 0,
+      BSRKey: map.id,
+      coverRaw: coverURL,
+    },
+  }));
 
   for (let i = 0; i < 4; i++) {
     await delay(400);
-    if (socket.readyState !== socket.OPEN) {
+    if (isSocketClosed()) {
       return;
     }
-    socket.send(
-      JSON.stringify({
-        _type: "event",
-        _event: "score",
-        scoreEvent: {
-          time: 0.01756902,
-          score: 9210,
-          accuracy: 0.9,
-          combo: 0,
-          missCount: 0,
-          currentHealth: 0.5,
-        },
-      }),
-    );
+    socket.send(JSON.stringify({
+      _type: "event",
+      _event: "score",
+      scoreEvent: {
+        time: 0.01756902,
+        score: 9210,
+        accuracy: 0.9,
+        combo: 0,
+        missCount: 0,
+        currentHealth: 0.5,
+      },
+    }));
   }
 
   await delay(400);
-  if (socket.readyState !== socket.OPEN) {
+  if (isSocketClosed()) {
     return;
   }
 
   const result = Math.random();
   const willFail = result < 2 / 3;
   const isNoFail = result < 1 / 3;
-  socket.send(
-    JSON.stringify({
-      _type: "event",
-      _event: "score",
-      scoreEvent: {
-        time: 0.01756902,
-        score: 19029,
-        accuracy: isNoFail ? 0.45 : 0.9,
-        combo: 4,
-        missCount: 0,
-        currentHealth: willFail ? 0 : 0.1,
-      },
-    }),
-  );
+  socket.send(JSON.stringify({
+    _type: "event",
+    _event: "score",
+    scoreEvent: {
+      time: 0.01756902,
+      score: 19029,
+      accuracy: isNoFail ? 0.45 : 0.9,
+      combo: 4,
+      missCount: 0,
+      currentHealth: willFail ? 0 : 0.1,
+    },
+  }));
 
   socket.send(`{ "_event": "pause", "_type": "event", "pauseTime": 2.11 }`);
   await delay(2000);
-  if (socket.readyState !== socket.OPEN) {
+  if (isSocketClosed()) {
     return;
   }
 
-  socket.send(
-    `{ "_type": "event", "_event": "gameState", "gameStateChanged": "Menu" }`,
-  );
+  socket.send(`{ "_type": "event", "_event": "gameState", "gameStateChanged": "Menu" }`);
   await delay(1000);
+
+  function isSocketClosed() {
+    return socket.readyState !== socket.OPEN;
+  }
+}
+
+function getRandomElement<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)]!;
 }
