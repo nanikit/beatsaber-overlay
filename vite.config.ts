dbd82d303bd895958b487b63624c0fc6a77e42c8
nanikit <nanikit4@gmail.com>
2024-03-17 04:51:39 +0900

    build: remove unused dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import cloudflarePagesFunctions from "vite-plugin-cloudflare-functions";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +17,5 @@ export default defineConfig({
         plugins: ["jotai/babel/plugin-react-refresh"],
       },
     }),
-    cloudflarePagesFunctions({ dts: "cloudflare.d.ts.log" }),
   ],
 });
