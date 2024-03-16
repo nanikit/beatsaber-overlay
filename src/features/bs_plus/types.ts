504eda08144820ec4deca3f7f516c1562e68ba35
nanikit <nanikit4@gmail.com>
2024-03-17 06:13:28 +0900

    refactor: separate types

---
+++ src/features/bs_plus/types.ts
@@ -0,0 +1,74 @@
+import { Difficulty } from "../../services/beatsaver";
+
+export type BsPlusMessage = Handshake | EventMessage;
+
+type Handshake = {
+  _type: "handshake";
+  protocolVersion: number;
+  gameVersion: string;
+  playerName: string;
+  playerPlatformId: string;
+};
+
+type EventMessage =
+  | GameStateEvent
+  | MapInfoEvent
+  | ResumeEvent
+  | PauseEvent
+  | ScoreEvent;
+
+type GameStateEvent = {
+  _type: "event";
+  _event: "gameState";
+  gameStateChanged: "Menu" | "Playing";
+};
+
+type ResumeEvent = {
+  _type: "event";
+  _event: "resume";
+  resumeTime: number;
+};
+
+type PauseEvent = {
+  _type: "event";
+  _event: "pause";
+  pauseTime: number;
+};
+
+type ScoreEvent = {
+  _type: "event";
+  _event: "score";
+  scoreEvent: {
+    time: number;
+    score: number;
+    accuracy: number;
+    combo: number;
+    missCount: number;
+    currentHealth: number;
+  };
+};
+
+type MapInfoEvent = {
+  _type: "event";
+  _event: "mapInfo";
+  mapInfoChanged: {
+    /** @example 'custom_level_1C2CC9A3F9880BC99A994A664E65D63CE8616DD0' */
+    level_id: string;
+    name: string;
+    sub_name: string;
+    artist: string;
+    mapper: string;
+    characteristic: "Standard";
+    difficulty: Difficulty;
+    /** milliseconds */
+    duration: number;
+    BPM: number;
+    PP: number;
+    BSRKey: "";
+    /** base64 */
+    coverRaw: string;
+    /** seconds */
+    time: number;
+    timeMultiplier: number;
+  };
+};
