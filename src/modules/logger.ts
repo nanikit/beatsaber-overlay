d7b9b495f5fecc1442f33a68572b9136b41e09b2
nanikit <nanikit4@gmail.com>
2024-11-12 06:58:25 +0900

    refactor: use environment variable

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -3,8 +3,7 @@ import { addBreadcrumb as sentryAddBreadcrumb, Breadcrumb, init } from "@sentry/
 const isLocal = location.hostname.match(/^(localhost|127\.0\.0\.\d+)$/);
 
 init({
-  dsn:
-    "https://dce8ee84b40c4538865653bb9f33e378@o4504300642959360.ingest.sentry.io/4504300651216896",
+  dsn: import.meta.env.VITE_SENTRY_DSN,
   tracesSampleRate: 0.1,
 });
 
